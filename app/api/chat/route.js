fbfd6405130edb3179cadb4a374ea1d095a66aba
cphu1829 <charlesphu18@yahoo.com>
2024-08-23 08:39:30 -0700

    first commit

---
+++ app/api/chat/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+import { Pinecone } from '@pinecone-database/pinecone'
+import OpenAI from 'openai'
+const systemPrompt = `
+You are a rate my professor agent to help students find classes, that takes in user questions and answers them.
+For every user question, the top 3 professors that match the user question are returned.
+Use them to answer the question if needed.'
+export async function POST(req) {
+  const data = await req.json()
+  // We'll add more code here in the following steps
+}
+  `
